320fcb7c82c3a14eb6250463654e419032e2b883
Oscar Portillo <ohportillo@hiberus.com>
2023-06-12 08:27:07 +0200

    feature oscarportillo

--- src/Shared/ValueObject/Url.php
+++ src/Shared/ValueObject/Url.php
@@ -4,5 +4,16 @@ namespace Symfony\Base\Shared\ValueObject;
 
 class Url extends StringValueObject
 {
+    public function __construct(string $value)
+    {
+        $this->validateUrl($value);
+        parent::__construct($value);
+    }
 
+    private function validateUrl(string $url): void
+    {
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            throw new \InvalidArgumentException('Invalid URL provided.');
+        }
+    }
 }
