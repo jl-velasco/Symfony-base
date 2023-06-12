0450c53325340519e161c710806ff9aa7666771e
Paco Martinez <fmartinez@hiberus.com>
2023-06-12 09:35:40 +0200

    Limited length Description to 1000

--- src/Shared/ValueObject/Description.php
+++ src/Shared/ValueObject/Description.php
@@ -1,9 +1,23 @@
 <?php
 declare(strict_types=1);
 
+
 namespace Symfony\Base\Shared\ValueObject;
 
 class Description extends StringValueObject
 {
+    const LEN_DESCRIPTION = '1000';
 
+    public function __construct(protected string $value)
+    {
+        parent::__construct($this->value());
+        $this->validate();
+    }
+
+    private function validate(): void
+    {
+        if (strlen($this->value)>= self::LEN_DESCRIPTION) {
+//            throw new InvalidValueException($this->value);
+        }
+    }
 }
