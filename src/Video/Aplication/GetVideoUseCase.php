b630030e492f2d78ee1dc44eacf76ee35cc9d16b
igorpuchkovcom <igor@puchkov.com>
2023-06-13 13:05:48 +0200

    Crear GetVideoUseCase

---
+++ src/Video/Aplication/GetVideoUseCase.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Symfony\Base\Video\Aplication;
+
+use Symfony\Base\Shared\Domain\ValueObject\Uuid;
+use Symfony\Base\Video\Aplication\VideoResponse;
+use Symfony\Base\Video\Domain\Exceptions\VideoNotExistException;
+use Symfony\Base\Video\Domain\VideoFinder;
+use Symfony\Base\Video\Domain\VideoRepository;
+
+class GetVideoUseCase
+{
+    public function __construct(
+        private readonly VideoFinder $finder
+    )
+    {
+    }
+
+    public function __invoke(string $id): VideoResponse
+    {
+        $video = $this->finder->__invoke(new Uuid($id));
+
+        return new VideoResponse(
+            $video->id()->value(),
+            $video->user_id()->value(),
+            $video->name()->value(),
+            $video->description()->value(),
+            $video->url()->value(),
+            $video->created_at()->value()
+        );
+    }
+}
