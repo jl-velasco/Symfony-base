f982eea15d264ca1b97cef949ba1ca77ef0ec5e3
Jorge <jorge.zgz@gmail.com>
2023-06-14 18:39:04 +0200

    Domain Events

--- src/Shared/Infrastructure/Bus/Event/InMemorySymfonyEventBus.php
+++ src/Shared/Infrastructure/Bus/Event/InMemorySymfonyEventBus.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Symfony\Base\Shared\Domain\Bus\Event;
+namespace Symfony\Base\Shared\Infrastructure\Bus\Event;
 
+use Symfony\Base\Shared\Domain\Bus\Event\DomainEvent;
+use Symfony\Base\Shared\Domain\Bus\Event\EventBus;
 use Symfony\Base\Shared\Infrastructure\Bus\CallableFirstParameterExtractor;
 use Symfony\Component\Messenger\Exception\NoHandlerForMessageException;
 use Symfony\Component\Messenger\Handler\HandlersLocator;
