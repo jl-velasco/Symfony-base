8587025effe3bf815424649f309148a66bc9d760
Jorge <jorge.zgz@gmail.com>
2023-06-14 18:38:41 +0200

    Domain Events

---
+++ src/Shared/Infrastructure/Bus/Event/InMemorySymfonyEventBus.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Base\Shared\Domain\Bus\Event;
+
+use Symfony\Base\Shared\Infrastructure\Bus\CallableFirstParameterExtractor;
+use Symfony\Component\Messenger\Exception\NoHandlerForMessageException;
+use Symfony\Component\Messenger\Handler\HandlersLocator;
+use Symfony\Component\Messenger\MessageBus;
+use Symfony\Component\Messenger\Middleware\HandleMessageMiddleware;
+
+class InMemorySymfonyEventBus implements EventBus
+{
+    private MessageBus $bus;
+
+    /** @param iterable<mixed> $subscribers */
+    public function __construct(iterable $subscribers)
+    {
+        $this->bus = new MessageBus(
+            [
+                new HandleMessageMiddleware(
+                    new HandlersLocator(CallableFirstParameterExtractor::forPipedCallables($subscribers))
+                ),
+            ]
+        );
+    }
+
+    public function publish(DomainEvent ...$events): void
+    {
+        foreach ($events as $event) {
+            try {
+                $this->bus->dispatch($event);
+            } catch (NoHandlerForMessageException) {
+            }
+        }
+    }
+}
