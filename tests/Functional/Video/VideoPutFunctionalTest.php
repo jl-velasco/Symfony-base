1b712cc03499bc881bf2df8b53336795b6c87c52
Paco Martinez <fmartinez@hiberus.com>
2023-06-22 18:00:51 +0200

    Frist Functional

---
+++ tests/Functional/Video/VideoPutFunctionalTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Symfony\Base\Tests\Functional\Video;
+
+use Doctrine\DBAL\Schema\Schema;
+use Symfony\Base\Tests\Fixtures\DB\VideoTableConnector;
+use Symfony\Base\Tests\Fixtures\Video\VideoMother;
+use Symfony\Base\Tests\Functional\FunctionalTestCase;
+
+class VideoPutFunctionalTest extends FunctionalTestCase
+{
+    public function testCreateVideoShouldOk() : void
+    {
+        $video = VideoMother::create()->random()->build();
+
+        $response = $this->doJsonRequest(
+            'PUT',
+            "/v1/video/" . $video->uuid()->value(),
+            [
+                'user_uuid' => $video->userUuid()->value(),
+                'name'=> $video->name()->value(),
+                'description' => $video->description()->value(),
+                'url' => $video->url()->value()
+            ]
+        );
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    protected function createTables(Schema $schema): void
+    {
+        VideoTableConnector::create($schema);
+    }
+}
