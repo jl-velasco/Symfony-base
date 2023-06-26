258ea726dd1c170015aa25fc914c26ecb3839144
Jorge <jorge.zgz@gmail.com>
2023-06-26 19:21:28 +0200

    CQRS

--- src/User/Aplication/DeleteUserCommandHandler.php
+++ src/User/Aplication/DeleteUserCommandHandler.php
@@ -3,13 +3,14 @@ declare(strict_types=1);
 
 namespace Symfony\Base\User\Aplication;
 
+use Symfony\Base\Shared\Domain\Bus\Command\CommandHandler;
 use Symfony\Base\Shared\Domain\Bus\Event\EventBus;
 use Symfony\Base\Shared\Domain\ValueObject\Uuid;
 use Symfony\Base\User\Domain\Exceptions\UserNotExistException;
 use Symfony\Base\User\Domain\UserFinder;
 use Symfony\Base\User\Domain\UserRepository;
 
-class DeleteUserCommandHandler
+class DeleteUserCommandHandler implements CommandHandler
 {
     public function __construct(
         private readonly UserRepository $repository,
