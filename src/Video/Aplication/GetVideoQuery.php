ef109e21295314dca9a55434da654a62f070e474
fjgarcial <fjgarcial@hiberus.com>
2023-06-26 19:20:58 +0200

    deleteVideo and getVideo

---
+++ src/Video/Aplication/GetVideoQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Symfony\Base\Video\Aplication;
+
+use Symfony\Base\Shared\Domain\Bus\Query\Query;
+
+class GetVideoQuery implements Query
+{
+    public function __construct(
+        private readonly string $id
+    )
+    {
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+}
