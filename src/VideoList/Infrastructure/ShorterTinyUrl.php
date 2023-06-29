73ecfd7a36a3d2c8cf6af87019189eb60ecd1b60
fjgamez.clb <fjgamez.clb@grupoprisa.com>
2023-06-29 18:39:19 +0200

    Fix merge

--- src/VideoList/Infrastructure/ShorterTinyUrl.php
+++ src/VideoList/Infrastructure/ShorterTinyUrl.php
@@ -2,28 +2,28 @@
 
 namespace Symfony\Base\VideoList\Infrastructure;
 
+use Symfony\Base\Shared\Domain\Repository\HttpRepository;
+use Symfony\Base\Shared\Domain\ValueObject\HttpRequestType;
 use Symfony\Base\Shared\Domain\ValueObject\Url;
-use Symfony\Base\Shared\Domain\Infrastructure\Http\GuzzleClient;
 use Symfony\Base\VideoList\Domain\ShorterUrlRepository;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
-class ShorterTinyUrl implements ShorterUrlRepository, HttpRepository
+class ShorterTinyUrl implements ShorterUrlRepository
 {
 
     private const URL_SERVICE = 'https://tinyurl.com/api-create.php?url=';
 
     public function __construct(
-        private readonly GuzzleClient $client
+        private readonly HttpRepository $httpClient
     ) {
     }
 
     public function shortUrl(string $url): string
     {
         $apiUrl = new Url(self::URL_SERVICE . urlencode($url));
-        $urlShort = '';
 
         try {
-            return $this->httpClient->send(new Url($apiUrl));
+            return $this->httpClient->send(new Url($apiUrl), new HttpRequestType('GET'));
         } catch (HttpException $e) {
             throw new \Exception('Error al acortar la URL: ' . $e->getMessage());
         };
