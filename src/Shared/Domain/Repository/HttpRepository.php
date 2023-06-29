5c5635280507c4ec602eaf72cefc9048c9ef2b06
jcasalshiberus <jcasals@hiberus.com>
2023-06-29 18:26:49 +0200

    Add HttpRequestType value object

--- src/Shared/Domain/Repository/HttpRepository.php
+++ src/Shared/Domain/Repository/HttpRepository.php
@@ -3,10 +3,11 @@ declare(strict_types=1);
 
 namespace Symfony\Base\Shared\Domain\Repository;
 
+use Symfony\Base\Shared\Domain\ValueObject\HttpRequestType;
 use Symfony\Base\Shared\Domain\ValueObject\Url;
 
 interface HttpRepository
 {
-    public function send(Url $url): string;
+    public function send(Url $url, HttpRequestType $requestType): string;
 
 }
--- src/Shared/Infrastructure/Http/GuzzleClient.php
+++ src/Shared/Infrastructure/Http/GuzzleClient.php
@@ -5,15 +5,16 @@ namespace Symfony\Base\Shared\Infrastructure\Http;
 
 use GuzzleHttp\Client;
 use Symfony\Base\Shared\Domain\Repository\HttpRepository;
+use Symfony\Base\Shared\Domain\ValueObject\HttpRequestType;
 use Symfony\Base\Shared\Domain\ValueObject\Url;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class GuzzleClient implements HttpRepository
 {
-    public function send(Url $url): string
+    public function send(Url $url, HttpRequestType $requestType): string
     {
         $client = new Client();
-        $result = $client->request('GET', $url->value());
+        $result = $client->request($requestType->value(), $url->value());
         if ($result->getStatusCode() >= 400) {
             throw new HttpException($result->getStatusCode(), "HTTP error");
         }
