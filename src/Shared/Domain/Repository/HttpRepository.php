bb90c44fbd96d1a0fdced9058d54e3b6d5043696
jcasalshiberus <jcasals@hiberus.com>
2023-06-29 18:15:01 +0200

    Add custom shared GuzzleClient

---
+++ src/Shared/Domain/Repository/HttpRepository.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Symfony\Base\Shared\Domain\Repository;
+
+use Symfony\Base\Shared\Domain\ValueObject\Url;
+
+interface HttpRepository
+{
+    public function send(Url $url): string;
+
+}
---
+++ src/Shared/Infrastructure/Http/GuzzleClient.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Symfony\Base\Shared\Infrastructure\Http;
+
+use GuzzleHttp\Client;
+use Symfony\Base\Shared\Domain\Repository\HttpRepository;
+use Symfony\Base\Shared\Domain\ValueObject\Url;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class GuzzleClient implements HttpRepository
+{
+    public function send(Url $url): string
+    {
+        $client = new Client();
+        $result = $client->request('GET', $url->value());
+        if ($result->getStatusCode() === 200) {
+            return $result->getBody()->getContents();
+        }
+        throw new HttpException($result->getStatusCode(), "HTTP error");
+    }
+
+}
